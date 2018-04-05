f9e00ca6d405fb670c7838a91f0e543374056141
Lander Willem <lander.willem@uantwerpen.be>
2018-04-05 09:55:04 +0200

    Add new tutorial data and disabled "lazy loading" to obtain the tutorial data fully visible in the R environment.

--- R/flu_city_processing.R
+++ R/flu_city_processing.R
@@ -1,65 +1,71 @@
+############################################################################
+#
+# This file is part of the course material for "Individual-based Modelling
+# in Epidemiology: A practical introduction" by Wim Delva and Lander Willem.
+#
+############################################################################
 # "FLU CITY" PROCESSING
 #
-# Author: Lander Willem
-# Last update: 14/11/2017
 ###################################
 
-run_output_processing <- function(output_log_csv)
+# DEBUG CODE
+if(0==1){
+  rm(list=ls())
+  setup_ibm_workbench()
+  run_flu_city_processing(flu_city_log)
+}
+
+run_flu_city_processing <- function(sim_data_log)
 {
 
-  ## LOAD LOGFILE
-  exp_data <- read.table(file=output_log_csv,header=T,sep=' ',row.names=NULL,stringsAsFactors = F)
-  dim(exp_data)
+  # set plot panels
+  par(mfrow=c(2,3))
 
   ## INFECTIONS OVER TIME (days)
-  plot(table(exp_data$day),type='b',xlab='days',ylab='new infections',main='incidence (per day)')
+  plot(table(sim_data_log$day),type='b',xlab='days',ylab='new infections',main='incidence (per day)')
 
   ## INFECTIONS OVER TIME (hour of a day)
-  plot(table(exp_data$hour),type='b',xlab='hours',ylab='new infections',main='incidence (hour of the day)')
+  plot(table(sim_data_log$hour),type='b',xlab='time of day (hour)',ylab='new infections',main='incidence (time of day)')
   abline(v=7.5,lty=2)
   abline(v=16.5,lty=2)
   abline(v=20.5,lty=2)
 
   ## CONTEXT
-  barplot(table(exp_data$context),main='context',ylab='count')
+  barplot(table(sim_data_log$context),main='context',ylab='frequency')
 
   ## GET AGE CLASSES
-  range(exp_data$age)
-  exp_data$age_class <- cut(exp_data$age,c(0,18,90),right = FALSE)
-  exp_data$age_infector_class <- cut(exp_data$age_infector,c(0,18,90),right = FALSE)
+  range(sim_data_log$age)
+  age_class_threshold <- c(0,18,65,90)
+  sim_data_log$age_class <- cut(sim_data_log$age,age_class_threshold,right = FALSE)
+  sim_data_log$age_infector_class <- cut(sim_data_log$age_infector,age_class_threshold,right = FALSE)
 
   ## INFECTIONS: TOTAL
-  print('all infections')
-  print(table(exp_data$age_infector_class,exp_data$age_class,dnn=c('age infecter','age infected')))
+  print(table(sim_data_log$age_infector_class,sim_data_log$age_class,dnn=c('age infecter','age infected \t\t -- all infections --')))
 
   ## INFECTIONS: HOUSEHOLD
-  print('household & community infections')
-  sel <- exp_data$context == 'houses'
-  print(table(exp_data$age_infector_class[sel],exp_data$age_class[sel],dnn=c('age infector','age infected')))
+  sel <- sim_data_log$context == 'houses'
+  print(table(sim_data_log$age_infector_class[sel],sim_data_log$age_class[sel],dnn=c('age infector','age infected \t\t -- infections at home --')))
 
   ## INFECTIONS: SCHOOL
-  print('infections at school')
-  sel <- exp_data$context == 'schools'
-  print(table(exp_data$age_infector_class[sel],exp_data$age_class[sel],dnn=c('age infector','age infected')))
+  sel <- sim_data_log$context == 'schools'
+  print(table(sim_data_log$age_infector_class[sel],sim_data_log$age_class[sel],dnn=c('age infector','age infected \t\t -- infections at school--')))
 
   ## INFECTIONS: WORKPLACE
-  print('infections at work')
-  sel <- exp_data$context == 'workplaces'
-  print(table(exp_data$age_infector_class[sel],exp_data$age_class[sel],dnn=c('age infector','age infected')))
+  sel <- sim_data_log$context == 'workplaces'
+  print(table(sim_data_log$age_infector_class[sel],sim_data_log$age_class[sel],dnn=c('age infector','age infected \t\t -- infections at work --')))
 
   ## SECUNDARY CASES
-  barplot(table(table(exp_data$id_infector)),main='secundary cases')
+  barplot(table(table(sim_data_log$id_infector)),main='secundary cases',xlab='secundary cases',ylab='count')
 
   ## SECUNDARY CASES OVER TIME
-  exp_data$sec_cases <- NA
-  for(i in 1:dim(exp_data)[1])
+  sim_data_log$sec_cases <- NA
+  for(i in 1:dim(sim_data_log)[1])
   {
-    exp_data$sec_cases[i] <- sum(exp_data$id_infector == exp_data$id[i])
+    sim_data_log$sec_cases[i] <- sum(sim_data_log$id_infector == sim_data_log$id[i])
   }
+  boxplot(sim_data_log$sec_cases ~ sim_data_log$day,outline=F,xlab='days',ylab='secundary cases',main='secundary cases')
 
-  #plot(exp_data$day,exp_data$sec_cases)
-  boxplot(exp_data$sec_cases ~ exp_data$day,outline=F,xlab='time',ylab='secundary cases')
-
-  ## (BASIC) REPRODUCTION NUMBER ??
+  # set plot panels back to default
+  par(mfrow=c(1,1))
 
 }
